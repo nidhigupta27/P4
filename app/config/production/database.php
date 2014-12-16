06c6a0d63a93fbc8f3085aca18465bc75e27a660
Nidhi Gupta <nidhigupta02176@gmail.com>
2014-12-16 17:38:59 -0500

    Added production database file

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'P4',
+            'username'  => 'root',
+            'password'  => '47krex6nxr',
+        ),
+
+    ),
+
+);
